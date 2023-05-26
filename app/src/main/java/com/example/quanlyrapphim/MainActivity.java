20c48b489b69f7746b8a31c4209757fe619e2caf
Minh Chau <buitongminhchau01032002@gmail.com>
2023-05-26 14:20:31 +0700

    [film list screen] add film list screen

--- app/src/main/java/com/example/quanlyrapphim/MainActivity.java
+++ app/src/main/java/com/example/quanlyrapphim/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.quanlyrapphim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnFilmList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFilmList = findViewById(R.id.btnFilmList);
+        btnFilmList.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, FilmListActivity.class));
+        });
     }
 }
