a5c70782c25c8abdfc95a3c3743300e3be5e3370
Minh Chau <buitongminhchau01032002@gmail.com>
2023-05-30 09:36:32 +0700

    update to androidx

--- app/src/main/java/com/example/quanlyrapphim/AddFilmActivity.java
+++ app/src/main/java/com/example/quanlyrapphim/AddFilmActivity.java
@@ -2,9 +2,8 @@ package com.example.quanlyrapphim;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
--- app/src/main/java/com/example/quanlyrapphim/FilmListActivity.java
+++ app/src/main/java/com/example/quanlyrapphim/FilmListActivity.java
@@ -2,11 +2,11 @@ package com.example.quanlyrapphim;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
