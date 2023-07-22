ef58cf47aca0d306c730f40eb8553d1c93bda3dc
Bùi Tống Minh Châu <buitongminhchau01032002@gmail.com>
2023-07-22 10:56:15 +0700

    Get view in film detail screen

--- app/src/main/java/com/example/quanlyrapphim/FilmDetailScreenFragment.java
+++ app/src/main/java/com/example/quanlyrapphim/FilmDetailScreenFragment.java
@@ -10,17 +10,30 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class FilmDetailScreenFragment extends Fragment {
 
-    private String _filmName;
+    private String argFilmName;
+    private ImageView imvImage;
+    private TextView tvName;
+    private TextView tvType;
+    private TextView tvCountry;
+    private TextView tvReleaseDate;
+    private TextView tvCast;
+    private TextView tvContent;
+    private MaterialButton btnEdit;
+    private MaterialButton btnDelete;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            _filmName = getArguments().getString("filmName");
+            argFilmName = getArguments().getString("filmName");
         }
     }
 
@@ -34,6 +47,17 @@ public class FilmDetailScreenFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Toast.makeText(getActivity(), _filmName, Toast.LENGTH_SHORT).show();
+        // GET VIEW
+        imvImage = view.findViewById(R.id.film_detail_screen_image);
+        tvName = view.findViewById(R.id.film_detail_screen_name);
+        tvType = view.findViewById(R.id.film_detail_screen_type);
+        tvReleaseDate = view.findViewById(R.id.film_detail_screen_release_date);
+        tvCast = view.findViewById(R.id.film_detail_screen_cast);
+        tvCountry = view.findViewById(R.id.film_detail_screen_country);
+        tvContent = view.findViewById(R.id.film_detail_screen_content);
+        btnEdit = view.findViewById(R.id.film_detail_screen_btn_edit);
+        btnDelete = view.findViewById(R.id.film_detail_screen_btn_delete);
+
+        tvName.setText(argFilmName);
     }
 }
