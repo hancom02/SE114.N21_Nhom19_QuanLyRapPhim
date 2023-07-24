6b980fa2ada2b6c0188b6655ccaf2e58332e58a5
ngochan <20520484@gm,uit.edu.vn>
2023-07-24 07:42:39 +0700

    changed in nav and MainActivity

--- app/src/main/java/com/example/quanlyrapphim/MainActivity.java
+++ app/src/main/java/com/example/quanlyrapphim/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         navController = navHostFragment.getNavController();
 
         // config app bar
-        appBarConfiguration = new AppBarConfiguration.Builder(R.id.ticketScreenFragment, R.id.filmScreenFragment, R.id.employeeScreenFragment).setOpenableLayout(drawerLayout).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.ticketScreenFragment, R.id.filmScreenFragment, R.id.employeeScreenFragment, R.id.cinemaRoomScreenFragment, R.id.refreshmentScreenFragment, R.id.timeSlotScreenFragment, R.id.showTimeScreenGroup).setOpenableLayout(drawerLayout).build();
 
         // config bottom navigation bar
         NavigationUI.setupWithNavController(bottomNavBar, navController);
