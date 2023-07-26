069dd3e28a74f60fba07f0624ffe76998a4caa39
ngochan <20520484@gm,uit.edu.vn>
2023-07-26 22:46:47 +0700

    fix model CinemaRoom and add CreateCinemaRoom

--- app/src/main/java/com/example/quanlyrapphim/models/CreateCinemaRoom.java
+++ app/src/main/java/com/example/quanlyrapphim/models/CreateCinemaRoom.java
@@ -1,2 +1,57 @@
-package com.example.quanlyrapphim.models;public class CreateCinemaRoom {
+package com.example.quanlyrapphim.models;
+
+import java.util.ArrayList;
+
+public class CreateCinemaRoom {
+    private String name;
+    private String type;
+    private double seatsColumn;
+    private double seatsRow;
+    private ArrayList<String> equipmentIds;
+
+    public CreateCinemaRoom(String name, double seatsRow, double seatsColumn){
+        this.name = name;
+        this.seatsRow = seatsRow;
+        this.seatsColumn = seatsColumn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getSeatsColumn() {
+        return seatsColumn;
+    }
+
+    public void setSeatsColumn(double seatsColumn) {
+        this.seatsColumn = seatsColumn;
+    }
+
+    public double getSeatsRow() {
+        return seatsRow;
+    }
+
+    public void setSeatsRow(double seatsRow) {
+        this.seatsRow = seatsRow;
+    }
+
+    public ArrayList<String> getEquipmentIds() {
+        return equipmentIds;
+    }
+
+    public void setEquipmentIds(ArrayList<String> equipmentIds) {
+        this.equipmentIds = equipmentIds;
+    }
 }
