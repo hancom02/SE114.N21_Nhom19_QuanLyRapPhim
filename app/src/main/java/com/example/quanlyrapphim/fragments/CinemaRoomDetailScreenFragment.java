4ddd44131f73193e316f9d50562b354724ab0682
ngochan <20520484@gm,uit.edu.vn>
2023-07-26 22:44:33 +0700

    fix cinemaRoom ui

--- app/src/main/java/com/example/quanlyrapphim/fragments/CinemaRoomDetailScreenFragment.java
+++ app/src/main/java/com/example/quanlyrapphim/fragments/CinemaRoomDetailScreenFragment.java
@@ -12,24 +12,30 @@ import androidx.fragment.app.Fragment;
 import com.example.quanlyrapphim.R;
 import com.google.android.material.button.MaterialButton;
 
-public class AddCinemaRoomScreenFragment extends Fragment {
-    private MaterialButton addCinemaRoomBtn;
-
+public class DetailCinemaRoomScreenFragment extends Fragment {
+    private MaterialButton editCinemaRoomBtn;
+//    private MaterialButton addCinemaRoomBtn;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        editCinemaRoomBtn = editCinemaRoomBtn.findViewById(R.id.item_cinemaroom_spinner_bt_edit);
+//        editCinemaRoomBtn.setVisibility(View.GONE);
+//
 //        addCinemaRoomBtn = addCinemaRoomBtn.findViewById(R.id.item_cinemaroom_spinner_bt_add);
-//        addCinemaRoomBtn.setVisibility(View.VISIBLE);
+//        addCinemaRoomBtn.setVisibility(View.GONE);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detail_edit_add_cinemaroom_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_cinemaroom_detail_screen, container, false);
 
-        addCinemaRoomBtn = view.findViewById(R.id.item_cinemaroom_spinner_bt_add);
-        addCinemaRoomBtn.setVisibility(View.VISIBLE);
+        editCinemaRoomBtn = view.findViewById(R.id.frg_cinemaroom_detail_btn_edit);
+        editCinemaRoomBtn.setVisibility(View.VISIBLE);
+
+//        addCinemaRoomBtn = view.findViewById(R.id.item_cinemaroom_spinner_bt_add);
+//        addCinemaRoomBtn.setVisibility(View.GONE);
 
         return view;
     }
@@ -38,7 +44,10 @@ public class AddCinemaRoomScreenFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+//        editCinemaRoomBtn = view.findViewById(R.id.item_cinemaroom_spinner_bt_edit);
+//        editCinemaRoomBtn.setVisibility(View.GONE);
+//
 //        addCinemaRoomBtn = view.findViewById(R.id.item_cinemaroom_spinner_bt_add);
-//        addCinemaRoomBtn.setVisibility(View.VISIBLE);
+//        addCinemaRoomBtn.setVisibility(View.GONE);
     }
 }
